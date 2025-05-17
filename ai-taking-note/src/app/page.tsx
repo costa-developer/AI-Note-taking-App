44bf3b7cce54d67aa4dcda0f7e9b15016127f652
Tendai <constantinegumunyu25@gmail.com>
2025-05-18 01:40:46 +0200

    Build LogOutButton and header

---
+++ ai-taking-note/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='h-20 bg-red-400 text-blue-300'>
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage
